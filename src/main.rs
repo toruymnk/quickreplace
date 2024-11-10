5d98fa5ea58d0ce8423dc17ba6b86554a5d6439b
Toru Yamanaka <toruymnk@gmail.com>
2024-11-10 23:28:54 +0900

    Regex

--- src/main.rs
+++ src/main.rs
@@ -39,6 +39,13 @@ fn parse_args() -> Arguments {
     }
 }
 
+use regex::Regex;
+
+fn replace(target: &str, replacement: &str, text: &str) -> Result<String, regex::Error> {
+    let regex = Regex::new(target)?;
+    Ok(regex.replace_all(text, replacement).to_string())
+}
+
 use std::fs;
 
 fn main() {
@@ -57,7 +64,15 @@ fn main() {
         }
     };
 
-    match fs::write(&args.output, data) {
+    let replaced_data = match replace(&args.target, &args.replacement, &data) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("{} failed to replace text: {:?}", "Error:".red().bold(), e);
+            std::process::exit(1);
+        }
+    };
+
+    match fs::write(&args.output, replaced_data) {
         Ok(_) => {}
         Err(e) => {
             eprintln!(
